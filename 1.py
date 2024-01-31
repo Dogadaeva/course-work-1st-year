c11fc5620b9008f0a86e0741856a3ee62978bd88
sofya <sofya.kvastsova@yandex.ru>
2024-01-31 13:45:06 +0300

    2 version

--- 1.py
+++ 1.py
@@ -4,6 +4,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Embedding, GRU
 from keras import utils
 from keras.preprocessing.sequence import pad_sequences
+
 import numpy as np
 import matplotlib.pyplot as plt
 
